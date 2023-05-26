9fabf9ff2f9441927bdb2b2fe9367fa696b0474b
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-05-26 19:28:19 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -13,12 +13,12 @@ class BST {
     Node* rt;
     Node* addNode(Node* rt, const T& value) {
         if (rt == nullptr) {
-            rt = new Node;
-            rt->value = value;
+		rt = new Node;
+		rt->value = value;
 		rt->left =  nullptr;
 		rt->right = nullptr;
-            rt->c = 1;
-	  } else if (rt->value < value) {
+		rt->c = 1;
+	} else if (rt->value < value) {
             rt->right = addNode(rt->right, value);
         } else if (rt->value > value) {
             rt->left = addNode(rt->left, value);
@@ -28,28 +28,30 @@ class BST {
         return rt;
     }
     Node* searchNode(Node* rt, const T& value) {
-        if (rt == nullptr || rt->value == value) 
-		return rt;
-        if (value < rt->value) 
-		return searchNode(rt->left, value);
-        return searchNode(rt->right, value);
+	    if (rt == nullptr || rt->value == value) 
+		    return rt;
+	    if (value < rt->value) 
+		    return searchNode(rt->left, value);
+	    return searchNode(rt->right, value);
     }
     int getDepth(Node* rt) {
         if (rt == nullptr) return 0;
-        return std::max(getDepth(rt->left), getDepth(rt->right));
+        return std::max(getDepth(rt->left), getDepth(rt->right)) + 1;
     }
 
  public:
     BST() : rt(nullptr) {}
     int depth() {
-        return getDepth(rt);
+        return getDepth(rt) - 1;
     void add(const T& value) {
         rt = addNode(rt, value);
     }
     int search(const T& value) {
        Node* t = searchNode(rt, value);
-       if (t != nullptr) return t->c;
-		else return  0;
+       if (t != nullptr)
+	       return t->c;
+       else return  0;
     }
 };
+	
 #endif  // INCLUDE_BST_H_
